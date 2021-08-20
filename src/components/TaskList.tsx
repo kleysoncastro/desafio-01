ce46921f878a4830eaa80a5643ac989f984e08d2
kleyson castro <kleysoncastro99@gmail.com>
2021-08-20 00:46:22 -0400

    :tada: Desafio concluido

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -14,16 +14,32 @@ export function TaskList() {
   const [tasks, setTasks] = useState<Task[]>([]);
   const [newTaskTitle, setNewTaskTitle] = useState('');
 
+  var date = new Date();
+
+
+
   function handleCreateNewTask() {
-    // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+   if (!newTaskTitle) return;
+    
+   const newTask = {
+     id: Math.random(),
+     title: newTaskTitle,
+     isComplete: false,
+   }
+   setTasks([...tasks, newTask]);
+   setNewTaskTitle('');
   }
 
   function handleToggleTaskCompletion(id: number) {
-    // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const edingTask = tasks.map(task => task.id === id ? {
+      ...task,
+      isComplete: !task.isComplete
+    } : task);
+    setTasks(edingTask);
   }
 
   function handleRemoveTask(id: number) {
-    // Remova uma task da listagem pelo ID
+    setTasks(tasks.filter(task => task.id !== id));
   }
 
   return (
